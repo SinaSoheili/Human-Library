cb34719d5cdcdb1d68c2bee985c15fef5a763194
Sina <sinasoheili79@gmail.com>
2023-03-09 10:13:34 +0330

    start project

---
+++ src/main/java/net/sinasoheili/humanlibrary/HumanLibrarayApplication.java
@@ -0,0 +1,13 @@
+package net.sinasoheili.humanlibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HumanLibrarayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HumanLibrarayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/sinasoheili/humanlibrary/HumanLibrarayApplicationTests.java
@@ -0,0 +1,13 @@
+package net.sinasoheili.humanlibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HumanLibrarayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
