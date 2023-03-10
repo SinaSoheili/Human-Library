ec88620ced7b5e32e094c7377e92790ff7dc6e4c
Sina <sinasoheili79@gmail.com>
2023-03-10 07:58:58 +0330

    rename application

--- src/main/java/net/sinasoheili/humanlibrary/config/HumanLibraryApplication.java
+++ src/main/java/net/sinasoheili/humanlibrary/config/HumanLibraryApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class HumanLibrarayApplication {
+public class HumanLibraryApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(HumanLibrarayApplication.class, args);
+		SpringApplication.run(HumanLibraryApplication.class, args);
 	}
 
 }
